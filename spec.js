d00d93459cf5111b9abd88ee6ba536752d558e24
Walmyr <wlsf82@gmail.com>
2014-10-28 14:15:34 -0200

    Updated tests for todomvc emberjs app.

--- spec.js
+++ spec.js
@@ -5,18 +5,21 @@ var todomvcHelper = require('./todomvc.helper');
 describe('angularjs todo mvc homepage', function() {
 
   it('should have a title', function() {
-    browser.get('http://todomvc.com/examples/angularjs/#/');
-    expect(browser.getTitle()).toEqual('AngularJS • TodoMVC');
+    browser.ignoreSynchronization = true;
+    browser.get('http://todomvc.com/examples/emberjs/');
+    expect(browser.getTitle()).toEqual('ember.js • TodoMVC');
   });
 
   todomvcHelper.addTodoItem('Teste de digitação em campo texto');
 
   it('should be able to add items in the to do list', function() {
-  var todoLabel = element(by.css('.view .ng-binding'));
-  expect(todoLabel.getText()).toEqual(text);
+    browser.ignoreSynchronization = true;
+    var todoLabel = element(by.css('#todo-list li label'));
+    expect(todoLabel.getText()).toEqual(text);
   });
 
   it('should be able to clean the to do list', function() {
+    browser.ignoreSynchronization = true;
     var toogleAllCheckBox = element(by.id('toggle-all'));
     var clearCompletedButton = element(by.id('clear-completed'));
     var viewDiv = element(by.className('view'));
@@ -28,9 +31,10 @@ describe('angularjs todo mvc homepage', function() {
   todomvcHelper.addTodoItem('teste');
 
   it('should be able to complete items and then add them to the completed list', function() {
-    var completedLink = element.all(by.css('a[ng-class="{selected: status == \'completed\'}"')).last();
+    browser.ignoreSynchronization = true;
+    var completedLink = element(by.css('.selected'));
     var toggleAll = element(by.id('toggle-all'));
-    var completedList = element(by.css('#todo-list .completed .view .ng-binding'));
+    var completedList = element(by.css('#todo-list .completed label'));
     toggleAll.click();
     completedLink.click();
     expect(completedList.getText()).toContain(text);
--- todomvc.helper.js
+++ todomvc.helper.js
@@ -3,6 +3,7 @@
 module.exports = {
   addTodoItem : function(item) {
     it('should add an item into the todo list', function() {
+      browser.ignoreSynchronization = true;
       var todoTextField = element(by.id('new-todo'));
       text = item;
       todoTextField.sendKeys(text);
