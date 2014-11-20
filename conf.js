272779fd1b458fb0e357cef3b51411c00a87cd80
Walmyr <wlsf82@gmail.com>
2014-11-20 01:43:34 -0200

    First tests for protractor-perf. Performance testing using protractor.

--- conf.js
+++ conf.js
@@ -1,5 +1,8 @@
 // conf.js
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js']
+  specs: ['spec.js'],
+  capabilities: {
+    'browserName': 'chrome'
+  }
 }
--- spec.js
+++ spec.js
@@ -1,28 +1,47 @@
 // spec.js
 
 var todomvcHelper = require('./todomvc.helper');
+var ProtractorPerf = require('protractor-perf');
 
 describe('angularjs todo mvc homepage', function() {
-
+  var perf = new ProtractorPerf(protractor);
   it('should have a title', function() {
     browser.get('http://todomvc.com/examples/angularjs/#/');
+    perf.start();
     expect(browser.getTitle()).toEqual('AngularJS • TodoMVC');
+    perf.stop();
+
+    if (perf.isEnabled) {
+      expect(perf.getStats('meanFrameTime')).toBeLessThan(30);
+    }
   });
 
   todomvcHelper.addTodoItem('Teste de digitação em campo texto');
 
   it('should be able to add items in the to do list', function() {
-  var todoLabel = element(by.css('.view .ng-binding'));
-  expect(todoLabel.getText()).toEqual(text);
+    var todoLabel = element(by.css('.view .ng-binding'));
+    perf.start();
+    expect(todoLabel.getText()).toEqual(text);
+    perf.stop();
+
+    if (perf.isEnabled) {
+      expect(perf.getStats('meanFrameTime')).toBeLessThan(30);
+    }
   });
 
   it('should be able to clean the to do list', function() {
     var toogleAllCheckBox = element(by.id('toggle-all'));
     var clearCompletedButton = element(by.id('clear-completed'));
     var viewDiv = element(by.className('view'));
+    perf.start();
     toogleAllCheckBox.click();
     clearCompletedButton.click();
     expect(viewDiv.isPresent()).toBe(false);
+    perf.stop();
+
+    if (perf.isEnabled) {
+      expect(perf.getStats('meanFrameTime')).toBeLessThan(30);
+    }
   });
 
   todomvcHelper.addTodoItem('teste');
@@ -31,9 +50,15 @@ describe('angularjs todo mvc homepage', function() {
     var completedLink = element.all(by.css('a[ng-class="{selected: status == \'completed\'}"')).last();
     var toggleAll = element(by.id('toggle-all'));
     var completedList = element(by.css('#todo-list .completed .view .ng-binding'));
+    perf.start();
     toggleAll.click();
     completedLink.click();
     expect(completedList.getText()).toContain(text);
+    perf.stop();
+
+    if (perf.isEnabled) {
+      expect(perf.getStats('meanFrameTime')).toBeLessThan(30);
+    }
   });
 
 });
