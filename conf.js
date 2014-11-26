d4987b679cbdf8db4378c75e169c31f269cd2b6e
Walmyr <wlsf82@gmail.com>
2014-11-26 01:14:43 -0200

    Added first tests with screenshot capturing.

--- conf.js
+++ conf.js
@@ -1,8 +1,16 @@
 // conf.js
+var ScreenShotReporter = require('protractor-screenshot-reporter');
+var HtmlReporter = require('protractor-html-screenshot-reporter');
+
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['spec.js'],
   capabilities: {
     'browserName': 'chrome'
+  },
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(new ScreenShotReporter({
+      baseDirectory: '/tmp/screenshots'
+    }));
   }
 }
--- spec.js
+++ spec.js
@@ -2,11 +2,23 @@
 
 var todomvcHelper = require('./todomvc.helper');
 var ProtractorPerf = require('protractor-perf');
+var fs = require('fs');
 
 describe('angularjs todo mvc homepage', function() {
+
+  function writeScreenShot(data, filename) {
+    var stream = fs.createWriteStream(filename);
+
+    stream.write(new Buffer(data, 'base64'));
+    stream.end();
+  }
+
   var perf = new ProtractorPerf(protractor);
   it('should have a title', function() {
     browser.get('http://todomvc.com/examples/angularjs/#/');
+    browser.takeScreenshot().then(function (png) {
+      writeScreenShot(png, 'exception.png');
+    });
     perf.start();
     expect(browser.getTitle()).toEqual('AngularJS • TodoMVC');
     perf.stop();
@@ -21,6 +33,9 @@ describe('angularjs todo mvc homepage', function() {
 
   it('should verify if the main web elements are present in the page', function() {
     var newTodo = element(by.id('new-todo'));
+    browser.takeScreenshot().then(function (png) {
+      writeScreenShot(png, 'exception-1.png');
+    });
     expect(newTodo.isPresent()).toBe(true);
   });
 
@@ -28,6 +43,9 @@ describe('angularjs todo mvc homepage', function() {
 
   it('should be able to add items in the to do list', function() {
     var todoLabel = element(by.css('.view .ng-binding'));
+    browser.takeScreenshot().then(function (png) {
+      writeScreenShot(png, 'exception-2.png');
+    });
     perf.start();
     expect(todoLabel.getText()).toEqual(text);
     perf.stop();
@@ -44,6 +62,9 @@ describe('angularjs todo mvc homepage', function() {
     perf.start();
     toogleAllCheckBox.click();
     clearCompletedButton.click();
+    browser.takeScreenshot().then(function (png) {
+      writeScreenShot(png, 'exception-3.png');
+    });
     expect(viewDiv.isPresent()).toBe(false);
     perf.stop();
 
@@ -61,6 +82,9 @@ describe('angularjs todo mvc homepage', function() {
     perf.start();
     toggleAll.click();
     completedLink.click();
+    browser.takeScreenshot().then(function (png) {
+      writeScreenShot(png, 'exception-4.png');
+    });
     expect(completedList.getText()).toContain(text);
     perf.stop();
 
